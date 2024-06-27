a729f0791b13c2e6e39f775364c68458039d5653
Rutva Pandya <rutvapandya3@gmail.com>
2024-06-27 15:09:05 +0400

    Use port 5001

--- MyFarm/manage.py
+++ MyFarm/manage.py
@@ -15,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    sys.argv = ["manage.py", "runserver", "5001"]
     execute_from_command_line(sys.argv)
 
 
