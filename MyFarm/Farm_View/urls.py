a2bad197c4d839a780165c869990aed3dabec036
sophiereznik <reznik.sophie16@gmail.com>
2024-07-11 16:53:00 -0500

    Worked on displaying images in grid on Farm View.

--- MyFarm/Farm_View/urls.py
+++ MyFarm/Farm_View/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, farm_view, get_images
 
 urlpatterns = [
     path('', home, name='farm_view_home'),
+    path('farm_view/', farm_view, name='farm_view'),
+    path('images/', get_images, name='get_images'),
 ]
